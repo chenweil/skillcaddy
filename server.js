252cdeb2095b83850ea1130138bc8662665358d8
chenweil <604302709@qq.com>
2026-08-01 18:53:30 +0800

    feat(web): add missing favicon

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const appInfo = await readVersion(rootDir);
 const mimeTypes = {
   '.html': 'text/html; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
+  '.svg': 'image/svg+xml',
   '.js': 'text/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8'
 };
